03be09c434affa2b40e89467534a7d3d2ed295e3
Daniel Penalva <dkajah@gmail.com>
2019-06-04 09:17:26 -0300

    change in the import

--- AudioSpectrumPatchApproximation.py
+++ AudioSpectrumPatchApproximation.py
@@ -27,7 +27,7 @@ from sklearn.linear_model import OrthogonalMatchingPursuit
 from sklearn.decomposition import MiniBatchDictionaryLearning
 from sklearn.feature_extraction.image import extract_patches_2d
 from sklearn.feature_extraction.image import reconstruct_from_patches_2d
-from sklearn.decomposition import ProjectedGradientNMF
+from sklearn.decomposition import NMF as ProjectedGradientNMF
 from skimage.filter import gabor_kernel
 import bregman.suite as br 
 import pdb
